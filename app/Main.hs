f923730cb1d2926132adaefb6bf52c41e449542c
Alexey Gmitron <gmitron01@gmail.com>
2021-10-13 13:36:50 +0700

    minimal working server

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,36 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
 
-import Lib
+import Blaze.ByteString.Builder (copyByteString)
+import qualified Data.ByteString.UTF8 as BU
+import Data.Monoid
+import Network.HTTP.Types (status200)
+import Network.Wai
+import Network.Wai.Handler.Warp
 
-main :: IO ()
-main = someFunc
+main = do
+  let port = 3000
+  putStrLn $ "Listening on port " ++ show port
+  run port app
+
+app req respond = respond $
+  case pathInfo req of
+    ["yay"] -> yay
+    x -> index x
+
+yay =
+  responseBuilder status200 [("Content-Type", "text/plain")] $
+    mconcat $
+      map
+        copyByteString
+        ["yay"]
+
+index x =
+  responseBuilder status200 [("Content-Type", "text/html")] $
+    mconcat $
+      map
+        copyByteString
+        [ "<p>Hello from ",
+          BU.fromString $ show x,
+          "!</p>",
+          "<p><a href='/yay'>yay</a></p>\n"
+        ]
