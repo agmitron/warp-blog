2ef8ac2900993a3c0cc16bf385a63565f3d6e182
Alexey Gmitron <gmitron01@gmail.com>
2021-10-28 19:51:48 +0700

    fix: change hashed password encoding

---
+++ curl/users/post.sh
@@ -0,0 +1,8 @@
+curl -X POST http://localhost:3000/users \
+  -F "name=Alexey" \
+  -F "surname=Gmitron" \
+  -F "avatar_uri=1.png" \
+  -F "login=agmitron" \
+  -F "password=1231231123" \
+  -F "is_admin=true" \
+  -H "Content-Type: multipart/form-data"
